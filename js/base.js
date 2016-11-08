fb07083ba0f838d03b81690c38e00c1f7fc0452f
璠 陈 <761102308@qq.com>
2016-11-08 16:13:56 +0800

    Update base.js

--- js/base.js
+++ js/base.js
@@ -5,16 +5,16 @@
 		"use strict";
 
 		var defaults  = {
-			first: "首", //设置false则不显示，默认为false
-			last: "末", //设置false则不显示，默认为false  
+			first: false,, //设置false则不显示，默认为false
+			last: false,, //设置false则不显示，默认为false  
 			prev: "上一页", //设置为false,则不显示，默认为“上一页”
 			next: "下一页", //设置为false,则不显示，默认为“下一页”
 			numOrignClass: ".num", //分页样式
-			numHoverClass: ".num-active", //分页激活样式
+			numHoverClass: ".active", //分页激活样式
 			firstClass: ".first", //“首页”按钮样式
 			lastClass: ".last", //“尾页”按钮样式
-			prevClass: ".prev", //“上一页”按钮样式
-			nextClass: ".next", //“下一页”按钮样式
+			prevClass: ".active", //“上一页”按钮样式
+			nextClass: ".active", //“下一页”按钮样式
 			callback: function() {}
 		};
 
